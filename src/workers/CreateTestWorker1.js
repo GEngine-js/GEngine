6731cfd2825b5c78965137778951bacbd121d368
junwei.gu <junwei.gu@jiduauto.com>
2022-10-21 13:42:22 +0800

    delete file

--- src/workers/CreateTestWorker1.js
+++
@@ -1,9 +0,0 @@
-import defined from "../utils/defined";
-import createTaskProcessorWorker from './CreateTaskProcessorWorker'
-function createTestWorker1(parameters, transferableObjects){
-   if(defined(parameters)){
-      return {name:'lisi',age:23};
-   }
-   return {name:'lisi',age:23};
-}
-export default createTaskProcessorWorker(createTestWorker1);
