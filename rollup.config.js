7be9e24d935f035714299ac4dc960c1bd1c46192
junwei.gu <junwei.gu@jiduauto.com>
2022-10-29 21:16:47 +0800

    fix build bug

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,8 @@ import path from "path"
 import livereload from "rollup-plugin-livereload"
 import serve from "rollup-plugin-serve"
 import { terser } from "rollup-plugin-terser"
-import ts from "rollup-plugin-typescript2"
+import ts from "rollup-plugin-typescript2";
+import commonjs from '@rollup/plugin-commonjs';
  
 export default {
   input: "./src/index.ts", 
@@ -17,6 +18,7 @@ export default {
   plugins: [
     ts(),
     livereload(),
+    commonjs()
     // terser(),
     // serve({
     //   open: true,
