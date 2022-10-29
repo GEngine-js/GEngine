0e6aba8d1450e4d94a14f79c1cac0958ff349cca
junwei.gu <junwei.gu@jiduauto.com>
2022-10-29 15:23:33 +0800

    fix some bug

--- src/renderpipeline/IBaseRenderLine.ts
+++ src/renderpipeline/IBaseRenderLine.ts
@@ -2,5 +2,5 @@ import DrawCommand from "../render/DrawCommand"
 
 export default interface IBaseRenderLine{
    render():void
-   setRenderList(options:{opaque: DrawCommand[],transparent: DrawCommand[]):void
+   setRenderList(options:{opaque: DrawCommand[],transparent: DrawCommand[]}):void
 }
