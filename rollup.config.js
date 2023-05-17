3a9c26f503563ba8d93c37dab054bd91f097f0f0
TOM <tangmeng_720@126.com>
2023-05-17 11:33:25 +0800

    build(package.json): remove rollup-plugin-sourcemap

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,7 @@
 import path from "path";
-import serve from "rollup-plugin-serve";
-import { terser } from "rollup-plugin-terser";
+import terser from "@rollup/plugin-terser";
 import ts from "rollup-plugin-typescript2";
 import commonjs from "@rollup/plugin-commonjs";
-import sourcemaps from "rollup-plugin-sourcemaps";
 import { fileURLToPath } from "url";
 
 const { BUILD, BABEL } = process.env;
@@ -23,15 +21,5 @@ export default {
 		: {
 				exclude: "node_modules/**"
 		  },
-	plugins: [
-		ts(),
-		commonjs(),
-		production ? terser() : null,
-		production ? null : sourcemaps()
-		// serve({
-		//   open: true,
-		//   port: 8080,
-		//   openPage: "/public/index.html"
-		// }),
-	]
+	plugins: [ts(), commonjs(), production ? terser() : null]
 };
