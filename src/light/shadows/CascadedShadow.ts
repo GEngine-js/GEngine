feb40b21d0a34393e0c314398ff3ae6d16994e5a
TOM <tangmeng_720@126.com>
2023-07-06 17:47:13 +0800

    WIP(csm working): csm init

--- src/light/shadows/CascadedShadow.ts
+++ src/light/shadows/CascadedShadow.ts
@@ -1,8 +1,29 @@
+import Camera from "../../camera/Camera";
 import Vector2 from "../../math/Vector2";
 import { BaseShadow } from "./BaseShadow";
+import { CascadedFrustum } from "./CascadedFrustum";
 
+const defaultCascadedShadowOptions = {
+	shadowMapSize: new Vector2(1024, 1024),
+	maxCascades: 3,
+	cascadeMode: "practical",
+	maxFar: 100000
+};
 export class CascadedShadow extends BaseShadow {
-	constructor() {
-		super(new Vector2(1024, 1024), undefined);
+	mainFrustum: CascadedFrustum;
+	frustums: Array<CascadedFrustum>;
+	breaks: Array<number>;
+	constructor(options: CascadedShadowOptions) {
+		const cascadedShadowOptions = Object.assign(defaultCascadedShadowOptions, options);
+		const { shadowMapSize, camera } = cascadedShadowOptions;
+		super(shadowMapSize, camera);
+		this.mainFrustum = new CascadedFrustum();
+		this.frustums = [];
+		this.breaks = [];
 	}
+}
+
+interface CascadedShadowOptions {
+	camera: Camera;
+	shadowMapSize?: Vector2;
 }
